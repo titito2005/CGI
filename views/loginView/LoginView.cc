1b6ea0685f21ce10b9eae50b0432185a2054a093
titito2005 <luis.4.2000@hotmail.com>
2022-05-07 17:20:45 -0600

    Completando header y footer layout.

--- views/loginView/LoginView.cc
+++ views/loginView/LoginView.cc
@@ -94,42 +94,44 @@ void LoginView::printHTML(){
     cout << "Content-type:text/html\r\n\r\n";
     cout<<"<!doctype html>"<<endl;
     cout<<"<html lang='en'"<<endl;
-    cout<<"<head>"<<endl;
-        cout<<"<meta charset='utf-8'>"<<endl;
-        cout<<"<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"<<endl;
-        cout<<"<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>"<<endl;
-        cout<<"<link rel='stylesheet' type='text/css' href='/public/userLogin/userLogin.css'>"<<endl;
-        cout<<"<title>Login page</title>"<<endl;
-    cout<<"</head>"<<endl;
-    //PRINT HEADER
-    headerView->printHeaderHTML();
-    cout<<"<body class='d-flex justify-content-center'>"<<endl;
-        cout<<"<div class='card mt-20' style='width: 30rem;'>"<<endl;
-            cout<<"<div class='card-body'>"<<endl;
-            //SI HAY ERROR LO IMPRIMO COMO UN WARNING
-            if(error){
-                cout<<"<div class='alert alert-warning' role='alert'>"<<endl;
-                    cout<< errorMessage << endl;
+        cout<<"<head>"<<endl;
+            cout<<"<meta charset='utf-8'>"<<endl;
+            cout<<"<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"<<endl;
+            cout<<"<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>"<<endl;
+            cout<<"<link rel='stylesheet' type='text/css' href='/public/userLogin/userLogin.css'>"<<endl;
+            cout<<"<title>Login page</title>"<<endl;
+        cout<<"</head>"<<endl;
+        //PRINT HEADER
+        cout<<"<body>"<<endl;
+            headerView->printHeaderHTML();
+            cout<<"<div class='main-content'>"<<endl;
+                cout<<"<div class='card mt-20' style='width: 30rem;'>"<<endl;
+                    cout<<"<div class='card-body'>"<<endl;
+                    //SI HAY ERROR LO IMPRIMO COMO UN WARNING
+                    if(error){
+                        cout<<"<div class='alert alert-warning' role='alert'>"<<endl;
+                            cout<< errorMessage << endl;
+                        cout<<"</div>"<<endl;
+                    }
+                        cout<<"<form action='login' method='POST'>"<<endl;
+                            cout<<"<div class='form-group'>"<<endl;
+                                cout<<"<label for='inputEmail1'>Email</label>"<<endl;
+                                cout<<"<input name='userEmail' type='email' class='form-control' id='inputEmail1' placeholder='Ingrese su correo electrónico'>"<<endl;
+                            cout<<"</div>"<<endl;
+                            cout<<"<div class='form-group'>"<<endl;
+                                cout<<"<label for='inputPassword'>Contraseña</label>"<<endl;
+                                cout<<"<input name='userPassword' type='password' class='form-control' id='inputPassword' placeholder='Ingrese su contraseña'>"<<endl;
+                            cout<<"</div>"<<endl;
+                            cout<<"<button type='submit' class='btn btn-primary'>Iniciar sesión</button>"<<endl;
+                        cout<<"</form>"<<endl;
+                    cout<<"</div>"<<endl;
                 cout<<"</div>"<<endl;
-            }
-                cout<<"<form action='login' method='POST'>"<<endl;
-                    cout<<"<div class='form-group'>"<<endl;
-                        cout<<"<label for='inputEmail1'>Email</label>"<<endl;
-                        cout<<"<input name='userEmail' type='email' class='form-control' id='inputEmail1' placeholder='Ingrese su correo electrónico'>"<<endl;
-                    cout<<"</div>"<<endl;
-                    cout<<"<div class='form-group'>"<<endl;
-                        cout<<"<label for='inputPassword'>Contraseña</label>"<<endl;
-                        cout<<"<input name='userPassword' type='password' class='form-control' id='inputPassword' placeholder='Ingrese su contraseña'>"<<endl;
-                    cout<<"</div>"<<endl;
-                    cout<<"<button type='submit' class='btn btn-primary'>Iniciar sesión</button>"<<endl;
-                cout<<"</form>"<<endl;
             cout<<"</div>"<<endl;
-        cout<<"</div>"<<endl;
-        cout<<"<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>"<<endl;
-        cout<<"<script src='https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>"<<endl;
-        cout<<"<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>"<<endl;
-    cout<<"</body>"<<endl;
-    footerView->printFooterHTML();
+            footerView->printFooterHTML();
+            cout<<"<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>"<<endl;
+            cout<<"<script src='https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>"<<endl;
+            cout<<"<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>"<<endl;
+        cout<<"</body>"<<endl;
     cout<<"</html>"<<endl;
 }
 
