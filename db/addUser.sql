f4ad30f4c7923d572d8e24172612ad88900e1c13
ElvisBadillaMena <elvis.badilla@ucr.ac.cr>
2022-05-15 19:41:13 -0600

    corregir error de merger

--- db/addUser.sql
+++ db/addUser.sql
@@ -1,7 +1,7 @@
 INSERT INTO user (firstName, lastName, email, userPassword) VALUES ('Luis', 'Rojas', 'luiscorreo', 'password');
 INSERT INTO user (firstName, lastName, email, userPassword) VALUES ('Eduardo', 'Carrillo', 'eduardocorreo', 'password2');
 INSERT INTO user (firstName, lastName, email, userPassword) VALUES ('Daniela', 'Rojas', 'danielacorreo', 'password3');
-INSERT INTO user (firstName, lastName, email, userPassword) VALUES ('Daniela', 'Rojas', 'prueba@prueba.com', 'prueba');
+INSERT INTO user (firstName, lastName, email, userPassword, phoneNumber, direction) VALUES ('Daniela', 'Rojas', 'prueba@prueba.com', 'prueba', '8888', 'hola');
 INSERT INTO sell (nameArticle, valueArticle, descriptionArticle, img) VALUES ('Zapatos', '1000', 'estos son unos zapatos talla 44','/home/elvis/proyecto/CGI/public/img/index.jpeg');
 INSERT INTO sell (nameArticle, valueArticle, descriptionArticle, img) VALUES ('Zapatos2', '1000', 'estos son unos zapatos talla 45','/home/elvis/proyecto/CGI/public/img/index.jpeg');
 INSERT INTO sell (nameArticle, valueArticle, descriptionArticle, img) VALUES ('Zapatos3', '1000', 'estos son unos zapatos talla 41','/home/elvis/proyecto/CGI/public/img/index.jpeg');
