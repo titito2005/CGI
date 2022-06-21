be226564af9099747845c5933c228f1c594e5aa1
Luis <luis.rojascarrillo@ucr.ac.cr>
2022-06-21 14:51:19 -0600

    prueba

--- services/parserService/ParserService.cc
+++ services/parserService/ParserService.cc
@@ -258,7 +258,7 @@ void ParserService::auditBuyProducts(bool isPurchase, string user, string ip, bo
     response = "Fail";
   }
 
-  myfile.open ((string(date)+"-sellData.txt"), std::ios::app);
+  myfile.open (("sellData.txt"), std::ios::app);
   myfile <<action<<" "<<user<<" "<<ip<<" "<<string(date)<<" "<<string(hour)<<" "<<response<<"\n";
   myfile.close();
 }
