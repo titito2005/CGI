a2428b2b9a26f7ddcfb91325a728545fc5c72573
Luis <luis.rojascarrillo@ucr.ac.cr>
2022-06-21 14:13:33 -0600

    Cambiando password db

--- services/Database.cc
+++ services/Database.cc
@@ -8,13 +8,13 @@ Database::~Database(){
     mysql_close(conn);
     delete conn;
 }
-        
+
 bool Database::connectToDatabase(){
     bool connected = true;
     const char* MY_HOSTNAME = "localhost";
     const char* MY_DATABASE = "server";
     const char* MY_USERNAME = "admin";
-    const char* MY_PASSWORD = "admin";
+    const char* MY_PASSWORD = "T1E2d3_2022";
     const char* MY_SOCKET = NULL;
 
     conn = mysql_init(NULL);
